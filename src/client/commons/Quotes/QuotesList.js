34a70081e815ca9ba36fb45fbd9fd2d14a7cea2a
Guy Meridor <guymeridor1@gmail.com>
2022-03-24 15:19:22 +0200

    translate and divider

--- src/client/commons/Quotes/QuotesList.js
+++ src/client/commons/Quotes/QuotesList.js
@@ -6,8 +6,10 @@ import List from '@material-ui/core/List';
 function QuotesList(props) {
     return <List>
         {props.quotes.map((quote, i) => <Quote key={quote.song.id} {...quote} />)
-            .reduce((acc, curr, idx) =>
-                acc.concat(curr, <Divider key={`d${idx}`} variant="inset" component="li" />), [])}
+            .reduce((acc, curr, idx, arr) =>
+                idx < arr.length - 1 ? 
+                acc.concat(curr, <Divider key={`d${idx}`} variant="inset" component="li" />) :
+                acc.concat(curr), [])}
     </List>
 }
 
--- src/server/translate/translateRouter.js
+++ src/server/translate/translateRouter.js
@@ -3,13 +3,12 @@ const router = express.Router();
 // const credentialsPath = './Song Analyzer-9cc26a513377.json';
 const credentialsPath = './Song Analyzer-9cc26a513377.json';
 process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath
-const credentials = require(`../../../${credentialsPath}`);
-const { projectId } = credentials;
+
 // Imports the Google Cloud client library
 const { Translate } = require('@google-cloud/translate');
 
 // Instantiates a client 
-const translate = new Translate({ projectId });
+const translate = new Translate();
 
 router.post('/', async (req, res) => {
     const { text, target } = req.body;
